274f67593627d574e32862f2e79907a666fb3442
Richard Antao <richardmantao@gmail.com>
2021-03-18 23:58:08 -0400

    Added page templates

---
+++ src/templates/BookReview/index.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { Helmet } from "react";
+
+import Nav from "components/Nav";
+import Footer from "components/Footer";
+
+import "styles/global.scss";
+import "./styles.scss";
+
+/**
+ * Lowercase string and replace spaces with dashes
+ * @param {string} title title string
+ * @returns {string} href
+ */
+const titleToHref = title =>
+	title
+		.toLowerCase()
+		.replace(":", "")
+		.replace(" ", "-");
+
+const BookReview = ({
+	children,
+	description,
+	keywords,
+	title,
+	prevTitle,
+	nextTitle
+}) =>
+	<>
+		<Helmet>
+			<meta charSet="utf-8" />
+			<meta name="description" content={description} />
+			<meta name="keywords" content={keywords} />
+			<meta name="author" content="Richard Antao" />
+			<title>{title}</title>
+		</Helmet>
+		<Nav />
+		<main id="book-review">
+			<h1>{title}</h1>
+			<article>
+				{children}
+			</article>
+			<aside>
+				<div>
+					<h4>Previous</h4>
+					<a href={`/books/${titleToHref(prevTitle)}`}>
+						{prevTitle}
+					</a>
+				</div>
+				<div>
+					Logo here
+				</div>
+				<div>
+					<h4>Next</h4>
+					<a href={`/books/${titleToHref(nextTitle)}`}>
+						{nextTitle}
+					</a>
+				</div>
+			</aside>
+		</main>
+		<Footer />
+	</>
+
+export default BookReview;
---
+++ src/templates/Page/index.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+
+import Nav from "components/Nav";
+import Footer from "components/Footer";
+
+const Page = ({
+	children,
+	id,
+	description,
+	keywords,
+	title
+}) =>
+	<>
+		<Helmet>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<meta name="description" content={description} />
+				<meta name="keywords" content={keywords} />
+				<meta name="author" content="Richard Antao" />
+				<meta name="application-name" content="richardantao.com" />
+				<title>{title}</title>
+			</Helmet>
+		</Helmet>
+		<Nav />
+		<main id={id} role="main">
+			{children}
+		</main>
+		<Footer />
+	</>;
+
+Page.propTypes = {
+	description: PropTypes.string.isRequired,
+	keywords: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired
+};
+
+export default Page;
