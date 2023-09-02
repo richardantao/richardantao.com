cf727035e0ed6788dc7d19cc4d81acc9118c368f
Richard Antao <richardmantao@gmail.com>
2023-09-02 17:19:13 -0400

    Update config

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 const nextConfig = {
 	reactStrictMode: true,
 	poweredByHeader: false,
-	output: "export",
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
@@ -20,7 +19,34 @@ const nextConfig = {
 		});
 
 		return config;
-	}
+	},
+	headers: async () => [
+		{
+			source: "/(.*)",
+			headers: [
+				{
+					key: "X-Frame-Options",
+					value: "SAMEORIGIN"
+				},
+				{
+					key: "X-XSS-Protection",
+					value: "1; mode=block"
+				},
+				{
+					key: "X-Content-Type-Options",
+					value: "nosniff"
+				},
+				{
+					key: "Referrer-Policy",
+					value: "same-origin"
+				},
+				{
+					key: "Permissions-Policy",
+					value: "camera=(), microphone=(), geolocation=(), interest-cohort=()"
+				}
+			]
+		}
+	]
 }
 
 module.exports = withBundleAnalyzer(nextConfig);
